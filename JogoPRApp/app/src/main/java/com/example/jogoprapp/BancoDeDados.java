522a3aad7caf18fd015c1e18717b2743be446145
Benedito Carvalho <benedito.carvalho@gmail.com>
2022-11-29 16:35:44 -0300

    Aula 3 (Parte 2) - App Jogo Perguntas e Respostas - Criado Estrutura de Banco de Dados utilizando biblioteca Room

---
+++ JogoPRApp/app/src/main/java/com/example/jogoprapp/BancoDeDados.java
@@ -0,0 +1,24 @@
+package com.example.jogoprapp;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+public abstract class BancoDeDados extends RoomDatabase {
+
+    private static BancoDeDados INSTANCE;
+
+    public static BancoDeDados getBancoDeDados(final Context context){
+        if (INSTANCE == null){
+            synchronized (BancoDeDados.class){
+                if (INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            BancoDeDados.class, "bd_questoes").allowMainThreadQueries()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
--- JogoPRApp/app/src/main/java/com/example/jogoprapp/MainActivity.java
+++ JogoPRApp/app/src/main/java/com/example/jogoprapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.jogoprapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JogarFragment fragment = new JogarFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment).commit();
     }
 }
